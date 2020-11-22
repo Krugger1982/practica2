6a35386a16bb72107e6758a7c4eb102149a93141
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-11-22 03:32:54 +0300

    Update Less1_recurcion.py

--- Less1_recurcion.py
+++ Less1_recurcion.py
@@ -3,7 +3,7 @@ def degree(N, M):
     if M == 0:
         return 1
     if M < 0:
-        return 1 / degree(N, -M)
+        return 1 / degree(N, -M)        # для отрицатеьных степеней поведение немного другое
     if M == 1:
         return N
     return N * degree(N, M - 1)
@@ -14,22 +14,25 @@ def figure_counter(N):
         return N
     return N % 10 + figure_counter(N // 10)
 
-
 def length(List):
+    List1 = List.copy()
+    return length_recoursive(List1)
+
+def length_recoursive(List):
     ''' Функция которая считает длину списка List'''
     try:
         List.pop(0)
-    except IndexError:
+    except IndexError:                  # отсекаем пустой список
         return 0
     else:
-        return 1 + length(List)
+        return 1 + length_recoursive(List)
 
 def palindrom(String):
     ''' Функция которая готовит строку к проверке и запускает проверку, является ли строка палиндромом'''
 
-    String = ''.join(String.split())            # избавимся от пробелов
-    String = String.lower()                     # и приведем все буквы к одному регистру
-    return palindr(String)
+    String1 = ''.join(String.split())               # избавимся от пробелов
+    String1 = String1.lower()                       # и приведем все буквы к одному регистру
+    return palindr(String1)
     
 def palindr(String):
     """ Рекурсивная функция проверяет является ли строка палидромом"""
@@ -40,13 +43,13 @@ def palindr(String):
 def even_numbered(List):
     """ Печатает четные числа из списка List"""
     try:
-        current = List.pop(0)
+        current = List[0]
     except IndexError:
         return
     else:
         if current % 2 == 0:
             print(current, end=' ')
-        even_numbered(List)
+        even_numbered(List[1:])
 
 def even_numbered_index(List, i=0):
     """ Печатает числа из списка List, находящиеся на четном индексе  """
@@ -67,9 +70,10 @@ def maximum_1(List):
 
 def maximum_2(List):
     """ Находит второе максимальное число в массиве """
-    Max = maximum_1(List)           # Находим максимальное число
+    List1 = List.copy()              # создадим копию списка чтоб не портить исходный, далее работаем с List1
+    Max = maximum_1(List1)           # Находим максимальное число
     if Max is None:
-        return Max
-    while maximum_1(List) == Max:
-        List.remove(Max)            # И удаляем все экземпляры этого числа из массива
-    return maximum_1(List)          # После удаления находим максимальное число из оставшихся
+        return Max                   # Для пустого массива вернется  None
+    while maximum_1(List1) == Max:
+        List1.remove(Max)            # И удаляем все экземпляры этого числа из списка
+    return maximum_1(List1)          # После удаления находим максимальное число из оставшихся
