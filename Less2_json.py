cac3dabe10a99122f29406300cc6ebb69d606c57
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-11-30 14:52:44 +0300

    Update Less2_json.py

--- Less2_json.py
+++ Less2_json.py
@@ -4,18 +4,18 @@ import requests
 response = requests.get("https://jsonplaceholder.typicode.com/todos")
 result = json.loads(response.text)
 with open ('json_file.json', 'a') as file:
-    Answers = {}
-    for i in result:
-        json.dump(i, file)                  # записываем все записи из ответа в файл
-        if i['userId'] not in Answers:      # Если найден новый пользователь
-            Answers[i['userId']] = {'Tasks':[], 'completed':0}       # Создаем для него новую запись в словаре
-            # (Ключ - имя польз., содержание - список оригиналтных задач и счетчик выполненных)
-        if i['id'] not in Answers[i['userId']]['Tasks'] and i['completed']:
-            Answers[i['userId']]['Tasks'].append(i['id'])
-            Answers[i['userId']]['completed'] += 1                      # и учитываем выполненную первую задачу
+    json.dump(result, file)                  # записываем все записи из ответа в файл
+Answers = {}
+for i in result:
+    if i['userId'] not in Answers:      # Если найден новый пользователь
+        Answers[i['userId']] = {'Tasks':[], 'completed':0}       # Создаем для него новую запись в словаре
+        # (Ключ - имя польз., содержание - список оригиналтных задач и счетчик выполненных)
+    if i['id'] not in Answers[i['userId']]['Tasks'] and i['completed']:
+        Answers[i['userId']]['Tasks'].append(i['id'])
+        Answers[i['userId']]['completed'] += 1                      # и учитываем выполненную первую задачу
             
-        elif i['id'] not in Answers[i['userId']]['Tasks']:               # а если ориг. задача не выполнена, то просто добавляем ее
-            Answers[i['userId']]['Tasks'].append(i['id'])
+    elif i['id'] not in Answers[i['userId']]['Tasks']:               # а если ориг. задача не выполнена, то просто добавляем ее
+        Answers[i['userId']]['Tasks'].append(i['id'])
 
 # ВЫВОД ОТВЕТОВ
 
