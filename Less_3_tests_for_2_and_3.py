488b0c2a03eefc20fbbed703bbb87db097247a4a
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-12-04 18:57:55 +0300

    Create Less_3_tests_for_2_and_3.py

---
+++ Less_3_tests_for_2_and_3.py
@@ -0,0 +1,24 @@
+import xml.etree.ElementTree as ETree
+import unittest
+
+
+class Tests_archives(unittest.TestCase):
+
+    def test_1(self):
+        xml1 = ETree.parse('demo.xml')
+        Testlist = ['name', 'age', 'sex', 'language', 'pc_item']                                                # список тегов, имеющих значнения
+        Results = [['Petya'], ['23'], ['true'], ['9', '7', '8'], ['Linux', 'Intel Core i7-8700', '64', '5000']] # список их значений
+        for i in range(len(Testlist)):
+            self.assertEqual(the_values_in_tag(Testlist[i],xml1),  Results[i])              # проверка - в соответствующем теге - соответствующие значения
+        self.assertEqual(the_values_in_tag('weight',xml1),  [])                             # проверка - поиск в несуществующем теге даст пустой список
+
+    def test_2(self):
+        xml1 = ETree.parse('demo.xml')
+        self.assertEqual(attribute_counter('name', xml1), 7)                # проверка - всего в файле 7 узлов с аттрибутом 'name'
+        Root = xml1.getroot()   
+        self.assertEqual(obhod2('name', Root[3]), 3)                        # а в ветке 'languages' - 3 таких узла
+        self.assertEqual(obhod2('name', Root[4]), 4)                        # а в ветке 'pc' - 4
+
+if __name__ == '__main__':
+    try: unittest.main()
+    except SystemExit: pass
