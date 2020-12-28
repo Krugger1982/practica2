71852d1e7185acca48b736d97f5b6009c303915d
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-12-28 16:21:42 +0300

    Update Less8_3.py

--- Less8_3.py
+++ Less8_3.py
@@ -8,11 +8,9 @@ url = 'https://api.vk.com/method/friends.getOnline?v=5.52&access_token=XXXXXXXXX
 # токен я по соображениям безопасности отсюда убрал
 
 response = requests.get(url)
-
 if response.status_code == 200:
-    with open('1.txt', 'wt') as page_file:
-        result = json.loads(response.text)
+    result = json.loads(response.text)
+    for keys in result:
+        print(keys, result[keys])
 else:
     print("ERROR",response.status_code)
-for keys in result:
-    print(keys, result[keys])
